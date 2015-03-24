25208a3dfaad975e1a866f261497b1aae5cf3450
IvanovAA <ivanovaa@ugsk.ru>
2015-03-24 08:46:12 +0500

    make one datafile. Add two more charts

--- pulse/js/app.js
+++ pulse/js/app.js
@@ -1,52 +1,81 @@
-d3.csv('data/dp.csv', function (data) {process_data(data, '#chart-dp-daily', '#chart-dp-summary')});
-d3.csv('data/ifl.csv', function (data) {process_data(data, '#chart-ifl-daily', '#chart-ifl-summary')});
-d3.csv('data/kasko.csv', function (data) {process_data(data, '#chart-kasko-daily', '#chart-kasko-summary')});
+var d3data = undefined,
+dateDimension = undefined,
+sourceDimension = undefined,
+productDimension = undefined,
+countDimension = undefined,
+dayGroup = undefined,
+weekGroup = undefined,
+minDate = undefined,
+maxDate = undefined,
+ndx = undefined;
 
-function process_data(data, dailyContainerId, summaryContainerId){
+d3.csv('data/pulse.csv', function(data){
+	d3data = data;
+	var format = d3.time.format.iso;
 
-	var parseDate = d3.time.format("%Y-%m-%d %H:%M:%S").parse;
-
-	data.forEach(function(d) {
-		d.date = parseDate(d.date);
+	d3data.forEach(function(d){
+		d.date = format.parse(d.date);
 	});
 
-	var ndx = crossfilter(data),
-	dateDimension = ndx.dimension(function(d) {return d.date;}),
-	dayGroup = dateDimension.group(function(d) {return d3.time.day(d);}),
+	ndx = crossfilter(d3data);
+	dateDimension = ndx.dimension(function(d) {return d.date;});
+	sourceDimension = ndx.dimension(function(d) {return d.source;});
+	productDimension = ndx.dimension(function(d) {return d.product;});
+	countDimension = ndx.dimension(function(d) {return d.count;});
+
+	dayGroup = dateDimension.group(function(d) {return d3.time.day(d);});
 	weekGroup = dateDimension.group(function(d) {return d3.time.week(d);});
-	
-	var minDate = dateDimension.bottom(1)[0].date;
-	var maxDate = dateDimension.top(1)[0].date;
 
-var volumeChart = dc.barChart('#chart-dp-volume');
+	minDate = dateDimension.bottom(1)[0].date;
+	maxDate = dateDimension.top(1)[0].date;
 
-	var summaryChart = dc.barChart(summaryContainerId)	
-	.width(820).height(240)
+	var volumeChart = dc.barChart('#chart-dp-volume')
+	.width(700).height(80)
+	.dimension(dateDimension)
+	.group(sourceDimension.filterAll('online'))
+	.centerBar(true)
+	.gap(1)
+	.x(d3.time.scale().domain([minDate,maxDate]))
+	.round(d3.time.week.round)
+	.xUnits(d3.time.weeks);
+
+
+	var summaryChart = dc.barChart('#chart-dp-summary')
+	.width(700).height(160)
 	.brushOn(false)
 	.dimension(dateDimension)
 	.group(dayGroup)
+	.centerBar(true)
+	.gap(1)
 	.x(d3.time.scale().domain([minDate,maxDate]))
 	.mouseZoomable(true)
 	.rangeChart(volumeChart)
-	.xUnits(d3.time.days)
-	.render();
+	.xUnits(d3.time.days);
 
-	volumeChart
-	.width(820).height(60)
+
+	var marketVolumeChart = dc.barChart('#chart-dp-volume-market')
+	.width(700).height(80)
 	.dimension(dateDimension)
 	.group(weekGroup)
 	.centerBar(true)
 	.gap(1)
 	.x(d3.time.scale().domain([minDate,maxDate]))
 	.round(d3.time.week.round)
-	.xUnits(d3.time.weeks)
+	.xUnits(d3.time.weeks);
 
-	.render();
 
-	var dailyNumber = dc
-	.numberDisplay(dailyContainerId)
+	var marketSummaryChart = dc.barChart('#chart-dp-market')
+	.width(700).height(160)
+	.brushOn(false)
+	.dimension(dateDimension)
 	.group(dayGroup)
-	.formatNumber(d3.format(".0s"))
-	.filter('2015-03-21')
-	.render();
-}
+	.centerBar(true)
+	.gap(1)
+	.x(d3.time.scale().domain([minDate,maxDate]))
+	.mouseZoomable(true)
+	.rangeChart(marketVolumeChart)
+	.xUnits(d3.time.days);
+
+
+	dc.renderAll();
+});
